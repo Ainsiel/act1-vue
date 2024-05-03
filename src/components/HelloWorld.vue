3316034e40b4ab760a9065a33d89bb250476cca9
Mahiiru <c.pavez07@ufromail.cl>
2024-05-03 15:01:12 -0400

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+
+const title = "Tutorial Vue Especializacion Tecnologica 1"
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <div>
+      <p>Contador: {{ count }}</p>
+      <button @click="increment">Incrementar</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+</style>
