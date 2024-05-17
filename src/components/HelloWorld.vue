8c84ca062722076deedf8fb1cb1fa52938d9f4a5
Mahiiru <c.pavez07@ufromail.cl>
2024-05-17 15:30:40 -0400

    Tutorial 2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,15 @@
-<script setup>
-import { ref } from 'vue';
-
-const title = "Tutorial Vue Especializacion Tecnologica 1"
-const count = ref(0)
-
-const increment = () => {
-  count.value++
-}
-</script>
-
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <div>
-      <p>Contador: {{ count }}</p>
-      <button @click="increment">Incrementar</button>
-    </div>
+  <div class="container">
+    <Padre />
   </div>
 </template>
 
+<script setup>
+import Padre from '@/components/Padre.vue'
+</script>
+
 <style scoped>
 h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
+  color: #3498db;
 }
 </style>
---
+++ src/components/MostrarMensaje.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <button @click="showMessage">Mostrar/Ocultar
+            mensaje</button>
+        <p v-if="mostrarMensaje">¡Hola, mundo!</p>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const mostrarMensaje = ref(false)
+
+const showMessage = () => {
+    mostrarMensaje.value = !mostrarMensaje.value
+}
+</script>
